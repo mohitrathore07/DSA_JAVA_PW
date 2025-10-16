6964397312423af55f82d3749cee5c958d500701
Mohit <mohitrathore8269@gmail.com>
2025-10-16 08:36:49 +0530

    arrayslist operations and questions

--- Arrays/DSA05_ArrayListIntro.java
+++ Arrays/DSA05_ArrayListIntro.java
@@ -1,5 +1,36 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class DSA05_ArrayListIntro {
-    
+    public static void main(String[] args) {
+        /*
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list); // [] - empty
+        System.out.println(list[0]); // if u haven't give size and print values using index it will give error
+        list.add(5);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        System.out.println(list); // we can direct print
+        */
+
+        ArrayList<Integer> list = new ArrayList<>(5); // size
+
+        list.add(0,10); // add on a specific index
+        list.add(1,10); 
+        list.add(2,10); 
+        list.add(3,10); 
+        list.add(4,10); 
+
+        System.out.println(list); 
+        
+        list.set(2, 200); // modify
+        System.out.println(list.get(2));  // get the value
+
+        list.remove(2);  // remove element from a specified index
+        System.out.println(list); 
+
+    }
 }
+
---
+++ Arrays/DSA06_QuestionsOnArrays.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class DSA06_QuestionsOnArrays {
+    public static void main(String[] args) {
+        int[] arr = {1,5,-6,5,8};
+        int target = 6;
+
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[i]+arr[j] == target) {
+                    System.out.println("i got it: "+arr[i]+" "+arr[j]);
+                    break;
+                }
+            }
+        }
+    }   
+}
