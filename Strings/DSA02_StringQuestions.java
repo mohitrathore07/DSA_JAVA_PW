74f02970c2f3eee4cd5d5a293332fcbb3d185eca
Mohit <mohitrathore8269@gmail.com>
2025-12-15 11:39:34 +0530

    string basics

---
+++ Strings/DSA02_StringQuestions.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class DSA02_StringQuestions {
+    public static void main(String[] args) {
+        String s = "Physics Wallah Skills";
+        String t = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if(i%2==0) t += "a";
+            else t += s.charAt(i);
+        }
+
+        System.out.println(t);
+    }
+}
